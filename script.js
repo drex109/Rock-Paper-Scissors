18cd6062e43940d7a640dd9b2308484ac5454833
drex109 <96746274+drex109@users.noreply.github.com>
2025-04-16 19:54:48 -0600

    removed psuedocode comments

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// FUNCTION returns rock, paper, or scissors
-    // assigns a random number between 1 & 3 to a variable
-    // IF random number is 1
-        // RETURN rock
-    // IF random number is 2  
-        // RETURN paper
-    // ELSE
-        // RETURN scissors
 function getCompChoice() {
     let randomNum = Math.floor((Math.random() * 3) + 1);
     if (randomNum === 1) {    
@@ -16,14 +8,7 @@ function getCompChoice() {
         return "scissors"
     }
 }
-// console.log(getCompChoice());
 
-// FUNCTION returns players choice of rock, paper, or scissors
-    // ask the user their choice and store in variable
-    // IF the players choice is a valid pick
-        // store that choice in a variable and return it
-    // ELSE
-        // return and error and try again
 function getHumanChoice() {
     let choice = prompt('rock, paper, or scissors?');
     if (choice.toLowerCase() == 'rock' 
@@ -35,18 +20,6 @@ function getHumanChoice() {
         return alert('Invalid input. Please try again.');
     }
 }
-// console.log(getHumanChoice());
-
-// console.log(playRound(getHumanChoice(), getCompChoice()))
-
-// FUNCTION plays game and keeps score
-    // variables for scores
-    // FOR loop up to 5
-        // call playRound function
-        // IF playRound is equal to win message
-            // add 1 to humanScore
-        // ELSE IF playRound is equal to lose message
-            // add 1 to compScore
 
 function playGame() {
     let humanScore = 0;
@@ -76,13 +49,6 @@ function playGame() {
         alert('Tie!')
     }
 
-// FUNCTION takes player choice and computer choice as arguments
-    // IF player choice is rock and comp choice is scissors
-    // OR player chose paper and comp chose rock
-    // OR player chose scissors and comp chose paper
-        // log player as winner
-    // ELSE
-        // log player as loser
     function playRound(humanChoice, compChoice) {
         if ((humanChoice == 'rock' && compChoice == 'scissors') 
             || (humanChoice == 'paper' && compChoice == 'rock')
