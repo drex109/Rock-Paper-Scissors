58a85b7906fff5a89d2f312c4609fc80bbc47466
drex109 <96746274+drex109@users.noreply.github.com>
2025-04-30 13:25:55 -0600

    finished UI elements for playing best of 5

--- script.js
+++ script.js
@@ -29,42 +29,48 @@ async function getHumanChoice() {
 async function playGame() {
     let humanScore = 0;
     let compScore = 0;
-    const choice = await getHumanChoice();
+    let humanScoreTxt = document.querySelector('#human-score');
+    let compScoreTxt = document.querySelector('#computer-score');
 
-    for (let i = 0; i < 5; i++) {
-        let roundResult = playRound(choice, getCompChoice());       
+    while (humanScore < 5 && compScore < 5) {
+        let roundResult = playRound(await getHumanChoice(), getCompChoice());       
         if (roundResult == 'You won the round!') {
-            humanScore++
-            alert('You won the round!');
+            humanScore++;
+            humanScoreTxt.textContent = humanScore;
         } else if (roundResult == 'You lost! Better luck next time!') {
-            compScore++
-            alert('Round lost! Better luck next time!');
-        } else {
-            alert('Draw!');
-        }    
+            compScore++;
+            compScoreTxt.textContent = compScore;    
+        }
     }
+
+    let gameTxt = document.querySelector('#game-results');
 
     console.log(humanScore);
     console.log(compScore);
     if (humanScore > compScore){
-        console.log('Congrats, you won the game! Your score: ' + humanScore +' Computer score: ' + compScore)
-        alert('Congrats, you won the game! Your score: ' + humanScore +' Computer score: ' + compScore);
+        console.log('Congrats, you won the game!');
+        gameTxt.style.color = 'green'
+        gameTxt.textContent = 'Congrats, you won the game!';
     } else if (compScore > humanScore) {
-        console.log('You lost the game! Your score: ' + humanScore +' Computer score: ' + compScore)
-        alert('You lost the game! Your score: ' + humanScore +' Computer score: ' + compScore);
+        console.log('You lost the game!');
+        gameTxt.style.color = 'red'
+        gameTxt.textContent = 'You lost the game!';
     } else {
-        console.log('Tie!')
-        alert('Tie!')
+        console.log('Tie!');
+        gameTxt.textContent = 'Tie!';
     }
 
     function playRound(humanChoice, compChoice) {
-        
+        let roundTxt = document.querySelector('#round-results');
+
         if ((humanChoice == 'rock' && compChoice == 'scissors') 
             || (humanChoice == 'paper' && compChoice == 'rock')
             || (humanChoice == 'scissors' && compChoice == 'paper')) {
                 console.log(humanChoice);
                 console.log(compChoice);
                 console.log('You won the round!');
+                roundTxt.style.color = 'green';
+                roundTxt.textContent = 'You won the round!';
                 return 'You won the round!';
         } else if ((compChoice == 'rock' && humanChoice == 'scissors') 
             || (compChoice == 'paper' && humanChoice == 'rock')
@@ -72,16 +78,18 @@ async function playGame() {
                 console.log(humanChoice);
                 console.log(compChoice);
                 console.log('You lost! Better luck next time!');
-                return 'You lost! Better luck next time!'
+                roundTxt.textContent = 'You lost! Better luck next time!';
+                roundTxt.style.color = 'red';
+                return 'You lost! Better luck next time!';
         } else {
             console.log(humanChoice);
             console.log(compChoice);
             console.log('Draw!');
+            roundTxt.style.color = 'black'
+            roundTxt.textContent = 'Draw!';
             return 'Draw!';
         }   
     }
-    
-    let resultTxt = document.querySelector('#results');
 
 }
 playGame();     
